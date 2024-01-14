3dd3ba5cf36bc2fd06257714e35f3068b70f9dec
Hericlis Ventura <hericlisventura.o@gmail.com>
2024-01-14 13:28:09 -0300

    deferentes formas de encontrar números, o valor da venda e da compra.

--- ProjetoDBSelects.sql
+++ ProjetoDBSelects.sql
@@ -17,11 +17,15 @@ select * from titulos order by cod_tit asc limit 3;
 select * from titulos order by cod_tit asc limit 0,3;
 select * from titulos where cod_tit <4;
 
-
 -- 2) codigo diferente de 2,6 e 9 
+select * from titulos where cod_tit <>2 and cod_tit <> 6 and cod_tit <>9 ;
+select * from titulos where cod_tit not in(2,6,9);
 
 -- 4) preço de venda entre 10 e 30 
+select * from titulos where val_cd between 100 and 150;
+select * from titulos where val_cd > 100 and val_cd <150;
 -- 5) preço de custo menor que 10
+select * from titulos where val_compra <100;
 -- 6) nome começado por consoante
 -- 7) nome que contenha o texo 'saber'
 -- 8) nome iniciado com 'acabou'
