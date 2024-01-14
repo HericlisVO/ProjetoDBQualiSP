e71fd9628b63c5c1035fbb19afe1a7ef51f58d19
Hericlis Ventura <hericlisventura.o@gmail.com>
2024-01-14 13:19:43 -0300

    formas diferentes de encontrar o código (id) 1, 2 e 3

---
+++ ProjetoDBSelects.sql
@@ -0,0 +1,33 @@
+-- conectando ao schema
+use db_cds;
+
+desc titulos;
+
+
+-- 1) Todos
+select * from titulos;
+-- 2) codigo 1, 2 ou 3
+select * from titulos where cod_tit =1 OR cod_tit =2 OR cod_tit =3;
+select * from titulos where cod_tit in(1,2,3);
+
+select * from titulos where cod_tit >=1 and cod_tit <=3;
+select * from titulos where cod_tit between 1 and 3;
+select * from titulos where cod_tit <=3;
+select * from titulos order by cod_tit asc limit 3;
+select * from titulos order by cod_tit asc limit 0,3;
+select * from titulos where cod_tit <4;
+
+
+-- 2) codigo diferente de 2,6 e 9 
+
+-- 4) preço de venda entre 10 e 30 
+-- 5) preço de custo menor que 10
+-- 6) nome começado por consoante
+-- 7) nome que contenha o texo 'saber'
+-- 8) nome iniciado com 'acabou'
+-- 9) lucro maior que 50%
+-- 10)lucro menor que 100%
+-- 11)lucro maior que 100.00
+
+
+
