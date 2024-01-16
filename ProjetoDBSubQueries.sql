c11263126a72f6b17d4a429f0e8d2303e78b033a
Hericlis Ventura <hericlisventura.o@gmail.com>
2024-01-16 09:36:30 -0300

    utilizando subquery dentro de subquery para finalizar os exercicios 30 e 31

--- ProjetoDBSubQueries.sql
+++ ProjetoDBSubQueries.sql
@@ -19,3 +19,27 @@ select nome_cli,
 	   'Casado'
 	from clientes where cod_cli not in(select cod_cli from conjuges)
     order by Cliente;
+    
+-- 28) Apresente todas as Categorias que estão sem CD
+
+select * from categorias where cod_cat not in(select distinct cod_cat from titulos);
+
+-- 29) Apresente apenas os Funcionarios que tem dependentes e que nunca atenderama nenhum pedido
+
+select * from funcionarios 
+			where cod_func in(select distinct cod_func from dependentes)
+            and cod_func not in (select distinct cod_func from pedidos)
+            ;
+            
+-- 30) Mostre todos os funcionarios que atenderam a clientes casados
+
+select nome_func as 'Funcionario' from funcionarios where cod_func in(
+			select distinct cod_func from pedidos where 
+				cod_cli in(select cod_cli from conjuges)) -- subquery dentro de subquery
+
+;
+
+-- 31) apresente os dados dos cds mais caros
+select * from titulos;
+select * from titulos order by val_cd DESC;
+select * from titulos  where val_cd =(select max(val_cd) from titulos);
