abeea3116c6e78f8f07f8a927fdcb5dd64ca8f12
Hericlis Ventura <hericlisventura.o@gmail.com>
2024-01-18 09:03:47 -0300

    Personalizando os  relatórios usando o case

--- ProjetoDBTotalizacao.sql
+++ ProjetoDBTotalizacao.sql
@@ -4,8 +4,14 @@ use  db_cds;
 
 -- 32) Quantos titulos possui cada artista do catalogo?
 select nome_art as 'Artistas',
-	   count(nome_cd) as 'Qtd_Titulos',
-       group_concat(nome_cd) as 'Tiutlos'
+		case
+	   when  count(nome_cd) > 0 then count(nome_cd)
+       else ''
+       end as 'Qtd_Titulos',
+       case
+       when group_concat(nome_cd) is not null then group_concat(nome_cd)
+       else 'Sem titulos'
+       end as 'Tiutlos'
 	   from artistas a
        left join titulos_artistas ta on a.cod_art = ta.cod_art
        left join titulos t on ta.cod_tit = t.cod_tit
@@ -28,7 +34,10 @@ select g.nome_grav as 'Gravadora',
 -- 34) Quantos artistas possui cada gravadora e qual o total de
 
 
-select g.nome_grav as 'Gravadora',
+select case
+		when g.nome_grav is not null then g.nome_grav
+        else 'Total : '
+        end as 'Gravadora',
 		count(nome_art) as 'Qtd_Artista',
 	    group_concat(nome_art) as 'Artista'
         from gravadoras g
@@ -36,7 +45,7 @@ select g.nome_grav as 'Gravadora',
             join titulos_artistas ta on t.cod_tit = ta.cod_tit
 			join artistas a on ta.cod_art = a.cod_art
 			group by g.nome_grav
-            with rollup ;
+            with rollup  ;
 
 
 -- 35) Liste todos os pedidos feitos no ano 2013 e qual o total faturado no ano
@@ -52,7 +61,7 @@ select case
           having sum( qtd_cd * val_cd) > 500
      -- with rollup
       union 
-      select 'Total',
+      select 'Total: ',
       sum( qtd_cd * val_cd)
       from  pedidos p 
         join titulos_pedidos tp on p.num_ped = tp.num_ped
@@ -63,8 +72,14 @@ select case
       
 -- 36) Liste todos os funcionarios e quantos dependentes cada um possui
 select nome_func as 'Funcionarios',
-		count( nome_dep) as 'número Dependentes',
-	group_concat(	nome_dep) as 'número Dependentes'
+		case 
+		when count( nome_dep) > 0 then concat('                   ', count( d.nome_dep))
+        else ''
+        end as 'Número Dependentes',
+	case
+     when group_concat(nome_dep) is not null then  group_concat(d.nome_dep)
+	 else 'Sem dependentes'
+     end as 'Nome Dependentes'
 		from funcionarios f 
         left join dependentes d on f.cod_func = d.cod_func
         group by nome_func;
